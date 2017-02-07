4c833957ad72681d534594ea41a2fd751077d792
liuwei <liuweinefu@qq.com>
2017-02-07 16:10:05 +0800

    20170207

--- routes/member.js
+++ routes/member.js
@@ -1,6 +1,6 @@
 var createRouter = require('./baseRouter');
-
-
+var multiparty = require('multiparty');
+var fs = require("fs");
 
 var config = {
     routerName: __filename,
@@ -9,7 +9,7 @@ var config = {
     mainIndex: 'multi', //multi or single
     importAble: true,
     exportAble: true,
-    exportExcelFields: ['name', 'pass', 'balance', 'phone', 'other_contacts', 'create_time', 'remark', 'member_role_id', 'member_role_name', 'discount'], //array
+    exportExcelFields: ['name', 'pass', 'balance', 'phone', 'other_contacts', 'create_time', 'remark', 'member_role_id', 'member_role_name', 'discount', 'member_case'], //array
     initArray: [{ db: 'member_role', fields: ['id', 'name'] }], // [{ db: 'user_role', fields: ['id', 'name'] }]
     dbTable: 'member',
     viewTable: 'view_member', //db or view
@@ -61,12 +61,16 @@ var config = {
             nullable: false, //默认ture
             formatter: 'int',
         })
-
         .set('discount', {
-        readonly: true, //默认false
-        //nullable: false, //默认ture
-        //formatter: 'string',
-    }),
+            readonly: true, //默认false
+            //nullable: false, //默认ture
+            //formatter: 'string',
+        })
+        .set('member_case', {
+            //readonly: true, //默认false
+            //nullable: false, //默认ture
+            formatter: 'string',
+        }),
 
 };
 
@@ -75,7 +79,57 @@ var router;
 [config, router] = createRouter(config);
 
 
+router.get('/case/:id', router.getCon, function(req, res, next) {
+    if (req.params.id === undefined) {
+        next();
+    }
 
+    req.dbCon.queryAsync('SELECT id,name,phone,other_contacts,remark,member_role_name,member_case FROM view_member WHERE id= ' + mysqlPool.escape(req.params.id))
+        .then(function(rows) {
+            req.session.currentMember = Object.assign({}, rows[0]);
+            req.session.currentMember.upPic = '';
+            req.session.currentMember.downPic = '';
+            req.session.currentMember.leftPic = '';
+            req.session.currentMember.rightPic = '';
+            res.render(router.getFileName(config.routerName, true) + 'list', req.session.currentMember);
+        })
+        .catch(function(err) {
+            next(err);
+        })
+        .finally(function() {
+            req.dbCon.release();
+        });
+
+});
+
+router.post('/case', function(req, res, next) {
+    if (req.session.currentMember === undefined) { next(err); };
+    var form = new multiparty.Form({ uploadDir: './public/tmp/' });
+
+    form.parse(req, function(err, fields, files) {
+        if (err) { next(err); };
+        console.log(files.files[0].originalFileName);
+
+        // fs.rename(files.upload[0].path, './public/memberCase/leftPic.jpg', function(err) {
+        //     if (err) { next(err); };
+        //     res.json({
+        //         err: false,
+        //         message: '上传成功'
+
+        //     });
+        // });
+
+        // console.log(fields);
+        // console.log(files.originalFilename);
+        // console.log(files.path);
+    });
+
+
+
+
+
+
+});
 
 //router的特例设置
 
