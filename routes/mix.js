32b06fa8c826def81b13e20a08f1c34c2e960373
liuwei <liuweinefu@qq.com>
2017-02-17 17:42:46 +0800

    20170217

--- routes/mix.js
+++ routes/mix.js
@@ -24,7 +24,7 @@ router.post('/', router.getCon, function(req, res, next) {
         return;
     }
 
-    req.dbCon.queryAsync('SELECT id,name,pass,phone,other_contacts,remark,member_role_name,member_case FROM view_member WHERE ' + mysqlPool.escapeId(req.body.name) + ' like "%' + req.body.value.trim() + '%"')
+    req.dbCon.queryAsync('SELECT id,name,pass,balance,phone,other_contacts,remark,member_role_name,member_case FROM view_member WHERE ' + mysqlPool.escapeId(req.body.name) + ' like "%' + req.body.value.trim() + '%"')
         .then(function(row) {
             if (row.length === 1) {
                 req.session.currentMember = row[0];
@@ -64,27 +64,52 @@ router.post('/check', router.getCon, function(req, res, next) {
             message: '无有效用户'
         });
         return;
+    };
 
-    }
-
-    if (!req.session.currentMember.pass) {
-        res.json({
-            err: false,
-        });
-        return;
-    }
-
-    if (md5(req.body.pass.trim()) !== req.session.currentMember.pass) {
+    if (req.session.currentMember.pass && md5(req.body.pass.trim()) !== req.session.currentMember.pass) {
         res.json({
             err: true,
             message: '密码错误'
         });
         return;
     }
-    res.json({
-        err: false,
-    });
+
+    let member = [];
+    member.push(['用户名', req.session.currentMember.name]);
+    member.push(['用户类型', req.session.currentMember.member_role_name]);
+    member.push(['余额', req.session.currentMember.balance]);
+    member.push(['电话', req.session.currentMember.phone]);
+    member.push(['其他联系方式', req.session.currentMember.other_contacts]);
+    member.push(['备注', req.session.currentMember.remark]);
+
+    req.dbCon.queryAsync('SELECT id,name,price,count FROM commodity ORDER BY id; SELECT id,name FROM user WHERE name!="admin" ORDER BY name')
+        .then(function(row) {
+            if (row[0].length < 1 || row[1].length < 1) {
+                res.json({
+                    err: true,
+                    message: '初始化错误'
+                });
+            } else {
+                res.json({
+                    err: false,
+                    member: member,
+                    comodity: row[0],
+                    user: row[1]
+                });
+            }
+        })
+        .catch(function(err) {
+            next(err);
+        })
+        .finally(function() {
+            req.dbCon.release();
+        });
 
 });
+
+router.post('/consumption', function() {
+
+
+})
 
 module.exports = router;
